02ed5d42761db2b4cb76585f8b8fd9d2eac23632
Shivam <shivamjagdishsingh@gmail.com>
2021-05-07 21:33:04 +0530

    ism

--- data_from_html.py
+++ data_from_html.py
@@ -9,48 +9,25 @@ import pathlib
 # writer = csv.writer(outfile)
 # writer.writerow(["name", "website"])
 
-categories = ["craft", "diy", "gadgets", "games", "gardentools", "greetings_and_stationary", "hair_care", "houseware",
-              "jewellery_and_watches", "kitchenware", "makeup", "pet_care", "skin_and_bodycare", "toys"]
+outfile = open("websitedata/ism/ism.csv", 'a+', newline='')
+writer = csv.writer(outfile)
+writer.writerow(["name", "website"])
+for i in range(2, 92):
+    filename = "websitedata/ism/" + str(i) + ".txt"
+    try:
+        with open(filename) as file:
+            soup = BeautifulSoup(file, "html.parser")
+        items = soup.body.find_all('div', class_="item")
 
-# categories = ["1", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
-#               "u", "v", "w", "x", "y", "z", "other"]
+        for item in items:
+            # breakpoint()
+            website = "https://www.ism-cologne.com" + item.find("a", class_="initial_noline")['href']
+            name = item.find("a", class_="initial_noline").text.strip("\n ")
+            print(website, name)
 
-for category in categories:
-    outfile = open("springfair" + "/" + "csv" "/" + category + '.csv', 'a+', newline='')
-    writer = csv.writer(outfile)
-    writer.writerow(["name", "address", "website"])
+            writer.writerow([name, website])
+        # print(category)
+    except FileNotFoundError:
+        print("changing category from ")
 
-    for i in range(1, 1000):
-        filename = "springfair" + "/" + category + "/" + str(i) + '.txt'
-        try:
-            with open(filename) as file:
-                soup = BeautifulSoup(file, "html.parser")
-            sb = soup.body.find('div', class_="p-tabs__body__content is-visible")
-            listhead = sb.find('div', class_="m-exhibitor-entry__item__body__contacts__address")
-            if listhead is not None:
-                # breakpoint()
-                address = listhead.text.strip("\n Address").replace('\n               ', ' ')
-            else:
-                address = "address"
-            listweb = sb.find('div', class_="m-exhibitor-entry__item__body__contacts__additional__website")
-            if listweb is not None:
-                # breakpoint()
-                website = listweb.text.strip("\n Website").replace('\n               ', ' ')
-            else:
-                website = "website"
-
-            listname = sb.find("div", class_="m-exhibitor-entry__item__body__contacts__logo--wrap")
-            if listname.a is not None:
-                # breakpoint()
-                name = listname.a.img['alt']
-            else:
-                name = "address"
-            print(website)
-
-            writer.writerow([name, address, website])
-            # print(category)
-        except FileNotFoundError:
-            print("changing category from ", category)
-            break
-
-    outfile.close()
+outfile.close()
--- get_html.py
+++ get_html.py
@@ -7,45 +7,30 @@ import errno
 
 browser = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
 
-browser.get("https://www.springfair.com")
+browser.get("https://www.ism-cologne.com/exhibitors-and-products/exhibitor-index/?fw_goto=aussteller/blaettern&fw_ajax=1&paginatevalues=&start=0")
 
-categories = [
-            # "craft", "diy", "gadgets", "games", "gardentools",
-              "greetings_and_stationary", "hair_care", "houseware",
-              "jewellery_and_watches", "kitchenware", "makeup", "pet_care", "skin_and_bodycare", "toys"]
 
-for category in categories:
+for w in range(10, 1771, 20):
+    browser.get("https://www.ism-cologne.com/exhibitors-and-products/exhibitor-index/?fw_goto=aussteller/blaettern&fw_ajax=1&paginatevalues=&start=" + str(w))
+    time.sleep(3)
+    html = browser.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    soup = soup.prettify()
+    # print(line_count, time.time())
+    # filename = "websitedata/ism/" + str(int((w+30)/20)) + '.txt'
 
-    with open("springfair" + "/" + category + '.csv') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
-        for row in csv_reader:
-            if line_count <= 0:
-                line_count += 1
-                continue
-            else:
-                try:
-                    browser.get(row[1])
-                    time.sleep(3)
-                    html = browser.page_source
-                    soup = BeautifulSoup(html, 'html.parser')
-                    soup = soup.prettify()
-                    print(line_count, time.time())
-                    filename = "springfair" + "/" + category + "/" + str(line_count) + '.txt'
+    # if not os.path.exists(os.path.dirname(filename)):
+    #     try:
+    #         os.makedirs(os.path.dirname(filename))
+    #     except OSError as exc:  # Guard against race condition
+    #         if exc.errno != errno.EEXIST:
+    #             raise
+    # with open(filename, 'w+') as file:
+    #     file.write(str(soup))
+    # file.close()
+    print((w+30)/20)
+# except AttributeError:
+#     print("issue")
+#     continue
 
-                    if not os.path.exists(os.path.dirname(filename)):
-                        try:
-                            os.makedirs(os.path.dirname(filename))
-                        except OSError as exc:  # Guard against race condition
-                            if exc.errno != errno.EEXIST:
-                                raise
-                    with open(filename, 'w+') as file:
-                        file.write(str(soup))
-                    file.close()
-                except AttributeError:
-                    print("issue")
-                    continue
-
-                line_count += 1
-        print(f'Processed {line_count} lines.')
 browser.close()
