2baa11550bd877d072a303cafa60b9f2d3e14c58
Shivam <shivamjagdishsingh@gmail.com>
2021-05-09 04:17:10 +0530

    outdoortradeshow

--- data_from_html.py
+++ data_from_html.py
@@ -4,30 +4,37 @@ import os
 import errno
 import pathlib
 
-# outfile = open('springfair_website.csv', 'a+', newline='')
+outfile = open('websitedata/outdoortradeshow/outdoortradeshow.csv', 'a+', newline='')
 
-# writer = csv.writer(outfile)
-# writer.writerow(["name", "website"])
-
-outfile = open("websitedata/ism/ism.csv", 'a+', newline='')
 writer = csv.writer(outfile)
-writer.writerow(["name", "website"])
-for i in range(2, 92):
-    filename = "websitedata/ism/" + str(i) + ".txt"
-    try:
-        with open(filename) as file:
-            soup = BeautifulSoup(file, "html.parser")
-        items = soup.body.find_all('div', class_="item")
+writer.writerow(["name", "website", "details"])
 
-        for item in items:
-            # breakpoint()
-            website = "https://www.ism-cologne.com" + item.find("a", class_="initial_noline")['href']
-            name = item.find("a", class_="initial_noline").text.strip("\n ")
-            print(website, name)
+filename = "websitedata/outdoortradeshow/outdoortradeshow.txt"
+try:
+    with open(filename) as file:
+        soup = BeautifulSoup(file, "html.parser")
+    items = soup.body.find_all('div', class_="text-cont")
 
-            writer.writerow([name, website])
-        # print(category)
-    except FileNotFoundError:
-        print("changing category from ")
+    for item in items:
+        try:
+            website = item.h4.a['href']
+        except:
+            website = "website"
+        try:
+            name = item.h4.a.text.strip('\t \n')
+        except:
+            name = item.h4.text.strip('\t \n')
+        try:
+            details = item.p.text
+        except:
+            details = "details"
+        print(
+            website, name,
+            details)
 
-outfile.close()
+        writer.writerow([name, website, details])
+    # print(category)
+except FileNotFoundError:
+    print("changing category from ")
+
+# outfile.close()
