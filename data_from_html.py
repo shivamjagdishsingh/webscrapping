4ca75e3a96bc698dae8afd2a20b4e30e8d91cee6
Shivam <shivamjagdishsingh@gmail.com>
2021-05-10 03:29:47 +0530

    Automechanika

--- data_from_html.py
+++ data_from_html.py
@@ -4,34 +4,38 @@ import os
 import errno
 import pathlib
 
-outfile = open('websitedata/nationaloutdoorexpo/nationaloutdoorexpo.csv', 'a+', newline='')
+# outfile = open('websitedata/nationaloutdoorexpo/nationaloutdoorexpo.csv', 'a+', newline='')
+#
+# writer = csv.writer(outfile)
+# writer.writerow(["name", "website"])
 
-writer = csv.writer(outfile)
-writer.writerow(["name", "website"])
-
-filename = "websitedata/nationaloutdoorexpo/nationaloutdoorexpo.txt"
+filename = "websitedata/automechanika/1.txt"
 try:
     with open(filename) as file:
         soup = BeautifulSoup(file, "html.parser")
-    items = soup.body.find_all('div', class_="logo type-exhibitor")
-
+    items = soup.find_all('li', class_="m-exhibitors-list__items__item--status-standard")
+    breakpoint()
     for item in items:
-        # breakpoint()
+        breakpoint()
         try:
-            website = item.div.a['href']
+            link = item.find('a', class_="m-exhibitors-list__items__item__header__title__link js-librarylink-entry")[
+                'href']
+
+        #     website = item.div.a['href']
         except:
             website = "website"
         try:
-            name = item.find('span', class_="title").text
+            name = item.find('a',
+                             class_="m-exhibitors-list__items__item__header__title__link js-librarylink-entry").text
         except:
             name = "name"
         # try:
         #     details = item.p.text
         # except:
         #     details = "details"
-        print(website, name)
+        # print(website, name)
         #
-        writer.writerow([name, website])
+        # writer.writerow([name, website])
     # print(category)
 except FileNotFoundError:
     print("changing category from ")
--- website_scrapper.py
+++ website_scrapper.py
@@ -9,67 +9,110 @@ from bs4 import BeautifulSoup
 import os
 import errno
 
-browser = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
-browser.get('https://www.nurseryfair.com/exhibitor_info.asp?keyletter=*&id=1901')
+links = [
+    'https://www.automechanika-birmingham.com/welcome/Exhibitors/exhibitor-list?&page=1&sortby=title%20asc%20%2Ctitle%20asc&searchgroup=943063BF-exhibitors',
+    'https://www.automechanika-birmingham.com/welcome/Exhibitors/exhibitor-list?&page=2&sortby=title%20asc%20%2Ctitle%20asc&searchgroup=943063BF-exhibitors',
+    'https://www.automechanika-birmingham.com/welcome/Exhibitors/exhibitor-list?&page=3&sortby=title%20asc%20%2Ctitle%20asc&searchgroup=943063BF-exhibitors',
+    'https://www.automechanika-birmingham.com/welcome/Exhibitors/exhibitor-list?&page=4&sortby=title%20asc%20%2Ctitle%20asc&searchgroup=943063BF-exhibitors',
+    'https://www.automechanika-birmingham.com/welcome/Exhibitors/exhibitor-list?&page=5&sortby=title%20asc%20%2Ctitle%20asc&searchgroup=943063BF-exhibitors',
+]
 
+browser = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
+# browser.get(
+#     'https://www.automechanika-birmingham.com/welcome/Exhibitors/exhibitor-list?&page=2&sortby=title%20asc%20%2Ctitle%20asc&searchgroup=943063BF-exhibitors')
+#
 outfile = open('websitedata/nationaloutdoorexpo/nationaloutdoorexpo_complete.csv', 'a+', newline='')
 writer = csv.writer(outfile)
 writer.writerow(
     ["name", "website", "details", "actual_website"])
 
-with open('websitedata/nationaloutdoorexpo/nationaloutdoorexpo.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count <= 0:
-            line_count += 1
-            continue
-            # print(f'Column names are {", ".join(row)}')
-        else:
-            try:
-                # browser.implicitly_wait(0)
-                browser.get(row[1])
-                # time.sleep(1)
-                # breakpoint()
-                data = browser.find_element_by_class_name('column')
+# with open('websitedata/automechanika/nationaloutdoorexpo.csv') as csv_file:
+#     csv_reader = csv.reader(csv_file, delimiter=',')
+#     line_count = 0
+#     for row in csv_reader:
+#         if line_count <= 0:
+#             line_count += 1
+#             continue
+#             # print(f'Column names are {", ".join(row)}')
+#         else:
+#             try:
+item_count = 0
+for i in links:
+    # browser.implicitly_wait(0)
+    browser.get(i)
+    # breakpoint()
 
-                # try:
-                #     address = " ".join([i for i in left_data.find_elements_by_tag_name('p')[0].text.split('\n') if
-                #                         "Web:" not in i and "Email" not in i and "Tel" not in i and "Fax" not in i])
-                # except:
-                #     address = "address"
-                #
-                # try:
-                #     telephone = \
-                #         [i for i in left_data.find_elements_by_tag_name('p')[0].text.split('\n') if "Tel:" in i][
-                #             0].strip(
-                #             'Tel: ')
-                # except:
-                #     telephone = "telephone"
-                try:
-                    website = data.find_element_by_tag_name('a').get_attribute('href')
-                except:
-                    website = "website"
-                # try:
-                #     email = [i for i in left_data.find_elements_by_tag_name('p')[0].text.split('\n') if "Email:" in i][
-                #         0].strip('Email: ')
-                # except:
-                #     email = "email"
-                try:
-                    d = data.find_element_by_tag_name('p').text
-                    if d != 'Go to website':
-                        details = d
-                    else:
-                        details = "details"
-                except:
-                    details = "details"
+    # items = browser.find_elements_by_class_name('m-exhibitors-list__items__item--status-standard')
+    link = browser.find_element_by_class_name('m-exhibitors-list__items__item__header__title__link')
 
-                print(details, website)
-                # breakpoint()
-                writer.writerow([row[0], row[1], website, details])
-            except Exception:
-                print("IIIIIIIIIIIIIIIIIIIIIIIIIIIII" + row[1])
-            line_count += 1
-    print(f'Processed {line_count} lines.')
+    link.click()
+    time.sleep(5)
+
+    for page in range(100):
+        time.sleep(5)
+        try:
+            name = browser.find_element_by_class_name("m-exhibitor-entry__item__header__title").text
+        except:
+            name = "name"
+        try:
+            description = browser.find_element_by_class_name("m-exhibitor-entry__item__body__description").text
+        except:
+            description = "description"
+        try:
+            website = browser.find_element_by_class_name(
+                "m-exhibitor-entry__item__body__contacts__additional__website").find_element_by_tag_name(
+                'a').get_attribute('href')
+        except:
+            website = "website"
+        try:
+            address = browser.find_element_by_class_name(
+                "m-exhibitor-entry__item__body__contacts__address").text.replace('\n', ' ')
+        except:
+            address = "address"
+        item_count += 1
+        print(item_count)
+        print(name, description, website, address)
+        writer.writerow([name, description, website, address])
+        # breakpoint()
+        browser.find_element_by_class_name("pagination__list__item__link--next").click()
+        time.sleep(2)
+
+        # try:
+        #     address = " ".join([i for i in left_data.find_elements_by_tag_name('p')[0].text.split('\n') if
+        #                         "Web:" not in i and "Email" not in i and "Tel" not in i and "Fax" not in i])
+        # except:
+        #     address = "address"
+        #
+        # try:
+        #     telephone = \
+        #         [i for i in left_data.find_elements_by_tag_name('p')[0].text.split('\n') if "Tel:" in i][
+        #             0].strip(
+        #             'Tel: ')
+        # except:
+        #     telephone = "telephone"
+        # try:
+        #     website = data.find_element_by_tag_name('a').get_attribute('href')
+        # except:
+        #     website = "website"
+        # # try:
+        # #     email = [i for i in left_data.find_elements_by_tag_name('p')[0].text.split('\n') if "Email:" in i][
+        # #         0].strip('Email: ')
+        # # except:
+        # #     email = "email"
+        # try:
+        #     d = data.find_element_by_tag_name('p').text
+        #     if d != 'Go to website':
+        #         details = d
+        #     else:
+        #         details = "details"
+        # except:
+        #     details = "details"
+
+        # print(details, website)
+        # breakpoint()
+    # except Exception:
+    # print("IIIIIIIIIIIIIIIIIIIIIIIIIIIII" + row[1])
+# line_count += 1
+# print(f'Processed {line_count} lines.')
 outfile.close()
 browser.close()
